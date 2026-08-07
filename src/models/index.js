72d0259d76763cce62217a165b10ce6769f25303
Sanmi Livingstone <workwithlivingstone@gmail.com>
2026-08-07 03:49:09 +0100

    fixed login bug

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,26 @@
 import { Sequelize, DataTypes } from 'sequelize';
+import { configDotenv } from "dotenv";
+configDotenv({ path: "../../.env" });
 
-const sequelize = new Sequelize('express-crud', 'postgres', 'mysecret', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  logging: false,
-});
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // This allows connections to databases with self-signed certificates
+      }
+    }
+  }
+);
+
 
 const User = sequelize.define('User', {
   id: {
