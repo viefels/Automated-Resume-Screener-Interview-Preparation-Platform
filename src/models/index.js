3c40de6201035dbfa49f17c639c92dc2fc23c404
Sanmi Livingstone <158168526+viefels@users.noreply.github.com>
2026-08-07 02:45:45 +0100

    Update Sequelize configuration to use environment variables
    Refactored Sequelize initialization to use environment variables for database configuration.

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,17 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize } from 'sequelize';
+import 'dotenv/config'; 
 
-const sequelize = new Sequelize('express-crud', 'postgres', 'mysecret', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 const User = sequelize.define('User', {
   id: {
